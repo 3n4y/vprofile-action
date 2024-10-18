afb0013531f49f6dd6d7dbbb5af7a1396a9e6904
3n4y <ndudi.obiki@gmail.com>
2024-10-18 16:07:01 -0600

    Update versions

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,21 +1,21 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.46.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.46.0"
     }
 
     random = {
-        source = "hashicorp/random"
-        version = "~> 3.5.1"
+      source  = "hashicorp/random"
+      version = "~> 3.5.1"
     }
   }
 
   backend "s3" {
     bucket = "vprofile-kops-endy-state"
-    key = "terraform.state"
+    key    = "terraform.state"
     region = "us-east-1"
   }
 
-  required_version = "~>1.6.3"
+  required_version = "~>1.9.3"
 }
