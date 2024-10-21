74573321d338f38c139561f98c79d9f8c7b64f34
3n4y <ndudi.obiki@gmail.com>
2024-10-21 10:22:27 -0600

    Complete terraform script

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -19,14 +19,35 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] #Canonical
 }
 
+resource "aws_security_group" "sg" {
+  name        = "instance-sg"
+  description = "Allow ssh traffic"
+  vpc_id      = module.vpc.default_vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_key_pair" "connect_key" {
   key_name   = "code_challenge"
   public_key = var.public_key
 }
 
 resource "aws_instance" "ubuntu" {
-  ami               = data.aws_ami.ubuntu.id
-  instance_type     = var.instance_type
-  availability_zone = var.zone1
-  key_name          = aws_key_pair.connect_key.key_name
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  availability_zone      = var.zone1
+  key_name               = aws_key_pair.connect_key.key_name
+  vpc_security_group_ids = [aws_security_group.sg.id]
 }
